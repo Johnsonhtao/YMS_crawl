8fd56579217ad7e957add8b7a7e595acad4d46a8
johnsonhtao <584271529@qq.com>
2020-11-04 15:00:22 +0800

    修改了deny 的正则表达式

--- new_amazon/new_amazon/settings.py
+++ new_amazon/new_amazon/settings.py
@@ -14,13 +14,13 @@ NEWSPIDER_MODULE = 'new_amazon.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 12
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -28,10 +28,10 @@ ROBOTSTXT_OBEY = True
 DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
-CONCURRENT_REQUESTS_PER_IP = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = True
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
--- new_amazon/new_amazon/spiders/amazon.py
+++ new_amazon/new_amazon/spiders/amazon.py
@@ -8,10 +8,15 @@ class AmazonSpider(CrawlSpider):
     allowed_domains = ['www.amazon.cn']
     start_urls = ['https://www.amazon.cn/gp/site-directory/ref=nav_deepshopall_variant_fullstore_l1']
 
+    # 定义提取url地址规则# LinkExtractor链接提取器，获取符合要求的url链接地址
+    # callback 提取出来的url地址的response会交给callback处理
+    # follow 当前url地址的相应是否重新经过Rules来提取url地址
     rules = (
-        Rule(LinkExtractor(allow='\/[^%s]*\_\=\w{20,40}'), callback='parse_item'),
+        Rule(LinkExtractor(deny=(r'/b\?ie=UTF8&node=\d+&ref_=\w{20,40}')), callback='parse_item'),
     )
 
+
     def parse_item(self, response):
-        item= response.xpath(".//a[contains(@class,'s-access-detail-page')]/@title").extract()
+        # 详情第一页的的名字
+        item = response.xpath(".//a[contains(@class,'s-access-detail-page')]/@title").extract()
         print(item)
